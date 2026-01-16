8064e68740569c6fe2909e6e3350d684c7a49fa2
AV - Security & Cryptography <250286626+av-crypto-security@users.noreply.github.com>
2026-01-16 06:52:38 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,8 +7,9 @@
  }
   
  provider "yandex" {
-   token  =  var.yc_token
+   service_account_key_file = var.sa_key_file
    cloud_id  = var.cloud_id
    folder_id = var.folder_id
    zone      = var.zone
+
  }
