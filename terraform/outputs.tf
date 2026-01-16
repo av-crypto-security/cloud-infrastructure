2696912925bdd612cbc2b2ceb98052e8ea230f14
AV - Security & Cryptography <250286626+av-crypto-security@users.noreply.github.com>
2026-01-16 13:43:49 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface[0].ip_address
+output "vm_internal_ip" {
+  value = module.vm.internal_ip
 }
- 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
+
+output "vm_external_ip" {
+  value = module.vm.external_ip
 }
