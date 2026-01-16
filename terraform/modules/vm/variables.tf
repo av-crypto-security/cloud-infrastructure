987961abbea4606d5d86cfbb44caa0b284866513
AV - Security & Cryptography <250286626+av-crypto-security@users.noreply.github.com>
2026-01-16 13:32:17 +0300

    Create variables.tf

---
+++ terraform/modules/vm/variables.tf
@@ -0,0 +1,15 @@
+variable "zone" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "ssh_public_key_path" {
+  type = string
+}
